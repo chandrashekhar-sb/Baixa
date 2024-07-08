850ef914b796f072bf4ab86bff808796ea993a9f
chandrashekhar-sb <sbiradarchandrashekhar@gmail.com>
2024-07-08 08:42:39 +0530

    First

---
+++ variables.js
@@ -0,0 +1,13 @@
+const accountID = 125445
+let accountEmail ="abc@gmail.com"
+var accountPassword = "123333"
+accountCity = "Jaipur"
+
+
+/* accountID = 114112 */ 
+accountEmail = "xxx@gmail.com"
+accountPassword = "666666"
+accountCity = "Bengaluru"
+
+console.log(accountID);
+console.table([accountID, accountEmail, accountPassword, accountCity]);
