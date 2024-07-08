679d913b6f4a076df90a81fb82d6c3c00bd410d2
chandrashekhar-sb <sbiradarchandrashekhar@gmail.com>
2024-07-08 12:35:55 +0530

    datatypes&conversions

---
+++ conversionOperations.js
@@ -0,0 +1,19 @@
+let score = "33a"
+
+console.log(typeof score);
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => Nan
+// true/false => 1/0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let num = 34
+let stringNumber = String(num)
+console.log(stringNumber);
+
---
+++ datatypes.js
@@ -0,0 +1,16 @@
+let name = "CS"
+let age = 20
+let isLogged = true
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// mull => standalone value
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof age);
+console.log(typeof "CS");
